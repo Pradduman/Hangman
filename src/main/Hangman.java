5f2bca03680396f6f4e697795c61854e5d8c2ca5
pradduman <pradduman.yadav_ccv17@gla.ac.in>
2019-01-10 20:42:41 +0530

    init commit

--- src/main/Hangman.java
+++ src/main/Hangman.java
@@ -1,5 +1,11 @@
 package main;
 
-public class Hangman {
+public class Hangman
+{
+    public static void main(String[] args) {
+        System.out.println("Enter the secret word");
+        Buffer
+        StringBuffer word;
 
+    }
 }
